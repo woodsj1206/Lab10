e8e7e5c68f1367b5e84831ab722b9072b87cf77d
Jonathan <jonwoods@umich.edu>
2022-04-13 21:23:29 -0400

    Fixed the if statement.

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,21 +3,21 @@ public class SelectionSort {
 /** Creates a new instance of SelectionSort */
     public SelectionSort() {
     }
-    
+
     /* A simple SelectionSort algorithm
-     * pre-condition: 
+     * pre-condition:
      * post-condition:
      * inputs:
      * outputs:
      * special conditions:
-     */    
+     */
     public int[] basicSelectionSort(int[] x) {
         for (int i = 0; i < x.length; ++i) {
             for (int j= i+1; j < x.length; ++j) {
                 if (x[i] > x[j]) {
                     temp = x[i];
                     x[i] = x[j];
-                    temp = x[j];
+                    x[j] = temp;
                 }
             } // end of inner for loop
         } // end of outer for loop
