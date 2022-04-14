610e1254570e557c36e5ff5a65d6654a35ae9653
Jonathan <jonwoods@umich.edu>
2022-04-13 21:27:19 -0400

    Added tests for mixed and duplicate numbers.

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -17,10 +17,10 @@ public class testSelectionSort {
 		testMixed();
 		testDuplicates();
 		}
-    
+
 	public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -28,30 +28,67 @@ public class testSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
         }
-    
-    
+
+
     public void testNegative(){
     	/** Test data contains negative values only **/
         }
-    
-    
-    public void testMixed(){
-    	/** Test data contains with both positive, negative and zeros **/
-        }
-    
-    
-    public void testDuplicates(){
-    	/** Test data contains duplicates **/
 
-        }
+
+		public void testMixed(){
+		        int[] arr = new int[5];
+		        arr[0] = 8;
+		        arr[1] = -9;
+		        arr[2] = 0;
+		        arr[3] = -10;
+		        arr[4] = 2;
+
+		        int[] Sortedarr = new int[5];
+		        Sortedarr[0] = -10;
+		        Sortedarr[1] = -9;
+		        Sortedarr[2] = 0;
+		        Sortedarr[3] = 2;
+		        Sortedarr[4] = 8;
+
+		        SelectionSort test = new SelectionSort();
+		        arr = test.basicSelectionSort(arr);
+
+		    	/** Test data contains with both positive, negative and zeros **/
+		        boolean sortedCorrectly = Sortedarr[0] == arr[0] && Sortedarr[1] == arr[1] && Sortedarr[2] == arr[2] && Sortedarr[3] == arr[3] && Sortedarr[4] == arr[4];
+		        assertTrue("testMixed did not sort the array correctly.", sortedCorrectly);
+					}
+
+
+		  public void testDuplicates(){
+		        int[] arr = new int[5];
+		        arr[0] = 7;
+		        arr[1] = 9;
+		        arr[2] = 7;
+		        arr[3] = 10;
+		        arr[4] = 2;
+
+		        int[] Sortedarr = new int[5];
+		        Sortedarr[0] = 2;
+		        Sortedarr[1] = 7;
+		        Sortedarr[2] = 7;
+		        Sortedarr[3] = 9;
+		        Sortedarr[4] = 10;
+
+		        SelectionSort test = new SelectionSort();
+		        arr = test.basicSelectionSort(arr);
+
+		    	/** Test data contains duplicates **/
+		        boolean sortedCorrectly = Sortedarr[0] == arr[0] && Sortedarr[1] == arr[1] && Sortedarr[2] == arr[2] && Sortedarr[3] == arr[3] && Sortedarr[4] == arr[4];
+		        assertTrue("testDuplicate did not sort the array correctly.", sortedCorrectly);
+					}
     }
